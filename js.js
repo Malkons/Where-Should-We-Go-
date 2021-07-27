137ab74c0fd72b293bbbc4b0f815be5da8ff1975
Malkons <marc.alkons@gmail.com>
2021-07-26 21:15:37 -0700

    removed a few console.logs, code clean up

--- js.js
+++ js.js
@@ -8,18 +8,11 @@ function pickRestaurant() {
     var placeToGo = restaurants[Math.floor(Math.random() * restaurants.length)];
     placeToGoArr.push(placeToGo);
     document.getElementById("placeToGo").innerHTML = "Place to go: " + placeToGoArr;
-    console.log(placeToGo);
-    console.log(placeToGoArr);
 };
 
-
-
-//pickRestaurant();
-
-//populate index with html 
+//Button 
 document.getElementById("clickMe").onclick = function () { pickRestaurant(); };
 
-
-
 //add restaurants
+
 //remove restaurants
