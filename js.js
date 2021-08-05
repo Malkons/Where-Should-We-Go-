7dafcffc58941d2b62b67d5adc021c50fa349be5
Malkons <marc.alkons@gmail.com>
2021-08-04 21:11:07 -0700

    will populate list with preloaded suggestions

--- js.js
+++ js.js
@@ -11,20 +11,23 @@ function pickRestaurant() {
     placeToGoArr.push(placeToGo);
     document.getElementById("pickedPlaceToGo").innerHTML = "Place to go: " + placeToGoArr;
 };
-function preLoadPlaces(){
+
+function preLoadPlaces() {
     var placeToGo = restaurants
-    var entry = document.createElement("li");
-    entry.appendChild(document.createTextNode(placeToGo));
-    entry.setAttribute("id", "item" + lastid);
-    entry.setAttribute("data-name", placeToGo);
-    var deleteButton = document.createElement("button");
-    deleteButton.appendChild(document.createTextNode("delete"));
-    deleteButton.setAttribute('onClick', 'deletePlace("' + 'item' + lastid + '")');
-    entry.appendChild(deleteButton);
-    lastid += 1;
-    list.appendChild(entry);
+    for (var i = 0; i < 4; i++) {
+        var entry = document.createElement("li");
+        entry.appendChild(document.createTextNode(placeToGo[i]));
+        entry.setAttribute("id", "item" + lastid);
+        entry.setAttribute("data-name", placeToGo[i]);
+        var deleteButton = document.createElement("button");
+        deleteButton.appendChild(document.createTextNode("delete"));
+        deleteButton.setAttribute('onClick', 'deletePlace("' + 'item' + lastid + '")');
+        entry.appendChild(deleteButton);
+        lastid += 1;
+        list.appendChild(entry);
+    };
 };
-preLoadPlaces();
+
 function populateListOfPlaces() {
     var placeToGo = document.getElementById("placeToGo").value;
     restaurants.push(placeToGo);
@@ -60,7 +63,7 @@ function getPlaces() {
     return places;
 };
 
-
+preLoadPlaces();
 //add restaurants
 // function addRestaurant() {
 //     var placeValue = document.getElementById("txtInput").value;
